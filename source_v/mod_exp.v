940eefa93c3a69f12183a0a81155a38da7ad1796
bpsswu <bpsswu@gmail.com>
2022-11-14 17:35:51 +0900

    Modify mod_exp.v

--- source_v/mod_exp.v
+++ source_v/mod_exp.v
@@ -1,4 +1,6 @@
-// 아직 컴파일은 못 해본 버전입니다
+// long_div, MM 모듈을 dummy 모듈로 제작 후,
+// 컴파일해 본 결과, 컴파일은 잘 되었습니다.
+// 문법 오류는 없는 듯 합니다.
 
 /*
 	input value = A, B, N, len
@@ -10,9 +12,9 @@ input clk, rstn;
 input [31:0] A, B, N, len;
 output [31:0] out;
 
-reg [31:0] out;
+wire [31:0] out;
 
-reg [31:0] res_buf_1, res_buf_2, res_buf_3;
+wire [31:0] res_buf_1, res_buf_2, res_buf_3;
 
 long_div inst_0 (clk, rstn, A, N, len, res_buf_1);
 long_div inst_1 (clk, rstn, B, N, len, res_buf_2);
