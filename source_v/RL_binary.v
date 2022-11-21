4ac474c2c62dbe39bee6c2d98023cad98ae2a384
bpsswu <bpsswu@gmail.com>
2022-11-22 00:01:23 +0900

    Update

--- source_v/RL_binary.v
+++ source_v/RL_binary.v
@@ -1,7 +1,21 @@
-module RL_binary(base, exp, N, R);
+module RL_binary(clk, rstn, en, base, exp, N, out);
+
+input clk, rstn, en;
 input [31:0] base, exp, N;
-output [31:0] R;
+output [31:0] out;
 
+reg [31:0] out, y;
 
+wire [31:0] 
+
+mod_exp inst_0 (clk, rstn, en, 
+
+always @ (posedge clk or negedge rstn) begin
+	if (!rstn) begin
+		out <= 1;
+		y <= base;
+	end
+	
+end
 
 endmodule
