60ec6534e42477a8da878de71e84421015007bfe
bpsswu <bpsswu@gmail.com>
2022-12-02 10:19:14 +0900

    Modify arduino project

--- arduino/from_arduino/from_arduino.v
+++ arduino/from_arduino/from_arduino.v
@@ -1,30 +1,55 @@
 module from_arduino (
-	clk, rstn, data, save,
-	rstn_led, save_led, comp, data_out);
+	clk, rstn, data, write, oe,
+	rstn_led, write_led, oe_led, comp1, comp2, comp3, data_out_led);
 	
-input clk, rstn, save;
-input [31:0] data;
-output rstn_led, save_led, comp;
-output [15:0] data_out;
+input			clk, rstn, write, oe;
+inout [31:0]	data;
+output			rstn_led, write_led, oe_led, comp1, comp2, comp3;
+output [15:0]	data_out_led;
 
-wire rstn_led, save_led;
-wire comp;
-wire [15:0] data_out;
-reg [31:0] register_1;
+wire rstn_led, write_led, comp1, comp2, comp3;
+wire [15:0] data_out_led;
 
-assign data_out = data[15:0];
-assign comp = (register_1 == 32'hF0F0_F0F0) ? 1 : 0;
+reg [31:0] register_1, register_2, register_3, register_4;
+reg [3:0] counter;
+
+assign data = oe ? register_4 : 32'bz;
 assign rstn_led = rstn;
-assign save_led = save;
+assign write_led = write;
+assign oe_led = oe;
+assign comp1 = (register_1 == 32'h0321_78C4) ? 1 : 0;
+assign comp2 = (register_2 == 32'h0000_0011) ? 1 : 0;
+assign comp3 = (register_3 == 32'h07A5_0679) ? 1 : 0;
+assign data_out_led = data[15:0];
 
-always @ (negedge save or negedge rstn) begin
+always @ (posedge write or negedge rstn) begin
 	if (!rstn) begin
 		register_1 <= 0;
+		register_2 <= 0;
+		register_3 <= 0;
+		register_4 <= 0;
+		counter <= 0;
 	end
 	else begin
-	
-		register_1 <= data;
-	
+		if (counter == 0) begin
+			counter <= counter + 1;
+		end
+		else if (counter == 1) begin
+			register_1 <= data;
+			counter <= counter + 1;
+		end
+		else if (counter == 2) begin
+			register_2 <= data;
+			counter <= counter + 1;
+		end
+		else if (counter == 3) begin
+			register_3 <= data;
+			counter <= counter + 1;
+		end
+		else if (counter == 4) begin
+			register_4 <= register_1 + register_2;
+			counter <= counter + 1;
+		end
 	end
 end
 
