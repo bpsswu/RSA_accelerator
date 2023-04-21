b65e004702e3f113800a65871f34d1bf23652a98
bpsswu <bpsswu@gmail.com>
2023-04-21 21:22:30 +0900

    Update IO.v

--- source_v/RSA32_v0.02/IO.v
+++ source_v/RSA32_v0.02/IO.v
@@ -6,25 +6,14 @@
 */
 module IO(
 	clk, rstn, write, oe, data,
-	led_rstn, led_write, led_oe, IO_end, comp1, comp2, comp3, comp4, counter_1);
+	IO_end, counter_1);
 	
 input clk, rstn, write, oe;
 inout [31:0] data;
-output wire led_rstn, led_write, led_oe;
 output reg IO_end;
-
 output [3:0] counter_1;
 
-output wire comp1, comp2, comp3, comp4;
-assign comp1 = (base == 32'h0321_78C4) ? 1 : 0;
-assign comp2 = (exponent == 32'h0000_0011) ? 1 : 0;
-assign comp3 = (modulus == 32'h07A5_0679) ? 1 : 0;
-assign comp4 = (result == 32'h007D_C743) ? 1 : 0;
-
 assign data = oe ? result : 32'bz;
-assign led_rstn = rstn;
-assign led_write = write;
-assign led_oe = oe;
 
 reg [31:0] base, exponent, modulus, result;
 reg [3:0] counter_1, counter_2;
@@ -40,6 +29,7 @@ RL_binary inst_rsa (
 	clk, rstn, rsa_start, base, exponent, modulus,
 	rsa_end, rsa_out);
 
+// “base, exp, mod” are input three times each from the outside.
 always @ (posedge write_p or negedge rstn) begin
 	if (!rstn) begin
 		base		<= 0;
@@ -66,6 +56,7 @@ always @ (posedge write_p or negedge rstn) begin
 	end
 end
 
+
 always @ (posedge clk or negedge rstn) begin
 	if (!rstn) begin
 		rsa_start	<= 0;
@@ -85,6 +76,8 @@ always @ (posedge clk or negedge rstn) begin
 		end
 		
 		if (rsa_end) begin
+			counter_1 <= 0;
+			counter_2 <= 0;
 			result <= rsa_out;
 			IO_end <= 1;
 		end
